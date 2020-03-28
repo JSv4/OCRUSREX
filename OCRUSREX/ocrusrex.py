5e65b22b3924bfc63da0a01d8951ebe8df25522d
JSv4 <scrudato@umich.edu>
2020-03-28 11:59:45 -0400

    Vastly improved system performance (a factor of 8X faster if you use byte-obj input+output instead of path input+output) using the multiprocess library. Can spin up 4 separate tesseract threads and break each pdf down into batches of 4. Higher threaded processors can support larger batches and even faster processing.

--- OCRUSREX/ocrusrex.py
+++ OCRUSREX/ocrusrex.py
@@ -2,114 +2,125 @@
 #Special thanks to @Abdou from https://stackoverflow.com/questions/47801564/is-it-possible-to-input-pdf-bytes-straight-into-pypdf2-instead-of-making-a-pdf-f
 #Special thanks to @Emile Bergeron from https://stackoverflow.com/questions/22795091/how-to-appeand-pdf-pages-using-pypdf2
 #Thanks very much for the guidance from http://www.blog.pythonlibrary.org/2013/07/16/pypdf-how-to-write-a-pdf-to-memory/
-#Some good (and largely unused
 
-import io, PyPDF2, pytesseract, tempfile, os
-
+import io, pytesseract, PyPDF2, os, time
+from multiprocess import Pool
 from pdf2image import convert_from_path, convert_from_bytes
 
-def OCRPDF(source="", targetPath=None, page=None, nice=5, verbose=False, tesseract_config='--oem 1 --psm 6 -l best/eng -c preserve_interword_spaces=1'):
+os.environ['OMP_THREAD_LIMIT'] = '1'
 
-    try:
+def ocrPilImage(image=None, nice=5, config=""):
+	ocred_page = pytesseract.image_to_pdf_or_hocr(image, extension='pdf', nice=nice, config=config)
+	return PyPDF2.PdfFileReader(io.BytesIO(ocred_page)).getPage(0)
 
-        output = PyPDF2.PdfFileWriter()
-        page_image_array = []
+def OCRPDF(source="", targetPath=None, page=None, nice=5, verbose=False, tesseract_config='--oem 1 -l eng -c preserve_interword_spaces=1 textonly_pdf=1'):
 
-        #if this is a string..
-        if isinstance(source, str):
+	try:
 
-            if verbose: print("You passed a string in as source. Trying this as source pdf file path.")
+		output = PyPDF2.PdfFileWriter()
 
-            filename, file_extension = os.path.splitext(source)
+		#if this is a string..
+		if isinstance(source, str):
 
-            if (file_extension == ".pdf"):
-                if verbose: print("OCRUSREX - Extracting Images from: {0}".format(source))
-                page_image_array = convert_from_path(source)
-            else:
-                if verbose: print("ERROR - sourcePath must be a file-like object with type "
-                     "pdf or must be a pdf with extension .pdf. Found: {0}".format(source))
-                return None
+			if verbose: print("You passed a string in as source. Trying this as source pdf file path.")
 
-        # IF source isn't a string, assume it's a file-like object. If incorrect, error handling will catch this.
-        else:
-            if verbose: print("OCRUSREX - Try extracting Images from file-like object")
-            page_image_array = convert_from_bytes(source)
+			page_count = PyPDF2.PdfFileReader(source).getNumPages()
+			filename, file_extension = os.path.splitext(source)
 
-        if verbose: print("OCRUSREX - Image extraction complete. Starting Tesseract 4.")
+			if (file_extension == ".pdf"):
+				if verbose: print("OCRUSREX - Try extracting Images from path: {0}".format(source))
 
-        #if a page number was provided, just process that page
-        if page:
-            if verbose: print("OCRUSREX - You specified a value of {0} for Page. If this is an integer,"
-                                " will extract only page with index = page. Otherwise, OCRUSREX will fail.".format(page))
-            output.addPage(PyPDF2.PdfFileReader(io.BytesIO(pytesseract.image_to_pdf_or_hocr(page_image_array[page], extension='pdf', nice=nice, config=tesseract_config))).getPage(0))
+				if page is None:
 
-        #Otherwise process the entire document
-        else:
-            for (count, image) in enumerate(page_image_array):
-                if verbose: print("\tTrying to OCR page {0}".format(count))
-                ocred_page = pytesseract.image_to_pdf_or_hocr(image, extension='pdf', nice=nice, config=tesseract_config)
+					if verbose: print("\tOCRing entire document with total page count of: {0}".format(page_count + 1))
 
-                if verbose: print("\tAdd page {0} to final pdf".format(count))
-                output.addPage(PyPDF2.PdfFileReader(io.BytesIO(ocred_page)).getPage(0))
+					for i in range(page_count):
+						if verbose: print("\tOCRing page {0} of {1}".format(i + 1, page_count + 1))
+						page_image_array = convert_from_path(source, dpi=300, first_page=i, last_page=i+1)
+						pdf_page = ocrPilImage(image=page_image_array[0], nice=nice, config=tesseract_config)
+						output.addPage(pdf_page)
 
-        if verbose: print("OCRUSREX - Successfully processed!")
+				else:
 
-        #If targetPath was provided, assume that it's a string and valid path. Try to write.
-        if targetPath:
-            outputStream = open(targetPath, "wb")
-            output.write(outputStream)
-            outputStream.close()
-            # upon success, return truthy values (in this case, True)
-            return True
+					if verbose: print("\tOCRing only page {0} of {1}".format(page, page_count + 1))
 
-        #otherwise, return results as bytes obj
-        else:
-            output_file_obj = io.BytesIO()
-            output.write(output_file_obj)
-            return output_file_obj.getvalue()
+					page_image_array = convert_from_path(source, dpi=300, first_page=page, last_page=page)
+					output.addPage(ocrPilImage(image=page_image_array[0], nice=nice, config=tesseract_config))
 
-    except Exception as e:
-        print("ERROR - Exception: {0}".format(e))
-        return None
+					if verbose: print("Done")
 
-def OCRPDF_tobytesobj(sourcePath="", page=None):
+		# IF source isn't a string, assume it's a file-like object. If incorrect, error handling will catch this.
+		else:
 
-    try:
-        if os.path.exists(sourcePath):
+			if verbose: print("OCRUSREX - Try extracting Images from bytes object")
+			page_count = PyPDF2.PdfFileReader(io.BytesIO(source)).getNumPages()
 
-            filename, file_extension = os.path.splitext(sourcePath)
+			if page is None:
 
-            if (file_extension==".pdf"):
+				if verbose: print("\tOCRing entire document with total page count of: {0}".format(page_count+1))
 
-                output = PyPDF2.PdfFileWriter()
+				for i in range(page_count):
+					if verbose: print("\tOCRing page {0} of {1}".format(i+1, page_count + 1))
+					page_image_array = convert_from_bytes(source, dpi=100, first_page=i, last_page=i+1)
+					output.addPage(ocrPilImage(image=page_image_array[0], nice=nice,
+					                           config=tesseract_config))
 
-                print("OCRUSREX - Extracting Images from: {0}".format(sourcePath))
-                page_image_array = convert_from_path('/home/jman/test_file.pdf')
-                print("OCRUSREX - Image extraction complete. Starting Tesseract 4.")
+			else:
+				if verbose: print("\tOCRing only page {0} of {1}".format(page, page_count + 1))
+				page_image_array = convert_from_bytes(source, dpi=100, first_page=page, last_page=page+1)
+				output.addPage(ocrPilImage(image=page_image_array[0], nice=nice,
+				                           config=tesseract_config))
 
-                for (count, image) in enumerate(page_image_array):
+		if verbose: print("OCRUSREX - Successfully processed!")
 
-                    print("\tTrying to OCR page {0}".format(count))
-                    ocred_page = pytesseract.image_to_pdf_or_hocr(image, extension='pdf')
+		#If targetPath was provided, assume that it's a string and valid path. Try to write.
+		if targetPath:
+			outputStream = open(targetPath, "wb")
+			output.write(outputStream)
+			outputStream.close()
+			# upon success, return truthy values (in this case, True)
+			return True
 
-                    print("\tConvert page {0} to pdf and append".format(count))
-                    output.addPage(PyPDF2.PdfFileReader(io.BytesIO(ocred_page)).getPage(0))
+		#otherwise, return results as bytes obj
+		else:
+			output_file_obj = io.BytesIO()
+			output.write(output_file_obj)
+			return output_file_obj.getvalue()
 
-                    print("\tSuccess!\n")
+	except Exception as e:
+		print("ERROR - Exception: {0}".format(e))
+		return None
 
-                output_file_obj = io.BytesIO()
-                output.write(output_file_obj)
+#Thanks to this guide for basic multithreading help: https://appliedmachinelearning.blog/2018/06/30/performing-ocr-by-running-parallel-instances-of-tesseract-4-0-python/
+def Multithreaded_OCRPDF(source="", targetPath=None, verbose=False):
 
-                return output_file_obj.getvalue()
+	if isinstance(source, str):
+		if verbose: print("You passed a string in as source. Trying this as source pdf file path.")
+		page_count = PyPDF2.PdfFileReader(source).getNumPages()
+	else:
+		if verbose: print("OCRUSREX - Try extracting Images from bytes object")
+		page_count = PyPDF2.PdfFileReader(io.BytesIO(source)).getNumPages()
 
-            else:
-                print("ERROR - sourcePath must be a file-like object with type "
-                      "pdf or must be a pdf with extension .pdf. Found: {0}".format(sourcePath))
-        else:
-            print("ERROR - Source path does not exist: {0}".format(sourcePath))
+	output = PyPDF2.PdfFileWriter()
 
-    except Exception as e:
-        print("ERROR - Unexpected error: {0}".format(e))
-        return None
+	p = Pool(4)
+	for ocred_page in p.map(lambda p: OCRPDF(source=source, verbose=verbose, page=p), range(page_count)):
+		output.addPage(PyPDF2.PdfFileReader(io.BytesIO(ocred_page)).getPage(0))
 
+	if verbose: print ("Multithreaded Execution Complete!")
 
+	# If targetPath was provided, assume that it's a string and valid path. Try to write.
+	if targetPath:
+		outputStream = open(targetPath, "wb")
+		output.write(outputStream)
+		outputStream.close()
+		# upon success, return truthy values (in this case, True)
+		return True
+
+	# otherwise, return results as bytes obj
+	else:
+		output_file_obj = io.BytesIO()
+		output.write(output_file_obj)
+		return output_file_obj.getvalue()
+
+	if verbose: print("Complete! Elapsed time: {0}".format(end-start))
--- tests/test.py
+++ tests/test.py
@@ -1,5 +1,7 @@
-from OCRUSREX.ocrusrex import OCRPDF
-import os, PyPDF2, tika
+import PyPDF2
+
+from OCRUSREX.ocrusrex import OCRPDF, Multithreaded_OCRPDF
+import os, tika, time
 tika.initVM()
 from tika import parser
 import Levenshtein
@@ -23,32 +25,78 @@ def extractPdfText(pdfPath, echo_ocred_content = False):
 
 dirname = os.path.dirname(__file__)
 source_file = os.path.join(dirname, "test.pdf")
+page_count = PyPDF2.PdfFileReader(source_file).getNumPages()
 
 #get the benchmark text to compare outputs against
 benchmark_text = open(os.path.join(dirname, "full_text.txt")).read()
 
-print("---Testing string source, byte obj return---")
+print("\n---Testing string source, byte obj return---")
+start = time.time()
 file_obj = OCRPDF(source=source_file)
 target_file = os.path.join(dirname, "rawrjr.pdf")
 with open(target_file, "wb+") as f:
     f.write(file_obj)
+end = time.time()
+print("\t Started: {0} | Ended: {1}. ELAPSED: {2} ({3} per page)".format(start, end, end-start, (end-start)/page_count))
 print("\t Levenshtein Distance (~Accuracy): {0}".format(Levenshtein.ratio(extractPdfText(target_file), benchmark_text)*100))
 if remove_test_output:
 	os.remove(target_file)
 
-print("---Testing string source, string target---")
+print("\n---Testing string source, string target---")
+start = time.time()
 target_file=os.path.join(dirname, 'lilrawr.pdf')
 OCRPDF(source=source_file, targetPath=target_file)
+end = time.time()
+print("\t Started: {0} | Ended: {1}. ELAPSED: {2} ({3} per page)".format(start, end, end-start, (end-start)/page_count))
 print("\t Levenshtein Distance (~Accuracy): {0}".format(Levenshtein.ratio(extractPdfText(target_file), benchmark_text)*100))
 if remove_test_output:
 	os.remove(target_file)
 
-print("---Testing byte obj source, byte obj return---")
+print("\n---Testing byte obj source, byte obj return---")
+start = time.time()
 target_file=os.path.join(dirname, "tinyrawr.pdf")
 with open(source_file,'rb') as s:
+	print("start")
 	file_obj = OCRPDF(source=s.read())
 	with open(target_file, "wb+") as f:
 		f.write(file_obj)
+end = time.time()
+print("\t Started: {0} | Ended: {1}. ELAPSED: {2} ({3} per page)".format(start, end, end-start, (end-start)/page_count))
+print("\t Levenshtein Distance (~Accuracy): {0}".format(Levenshtein.ratio(extractPdfText(target_file), benchmark_text)*100))
+if remove_test_output:
+	os.remove(target_file)
+
+print("\n---MULTITHREAD: Testing string source, byte obj return---")
+start = time.time()
+file_obj =  Multithreaded_OCRPDF(source=source_file)
+target_file = os.path.join(dirname, "rawrjr.pdf")
+with open(target_file, "wb+") as f:
+    f.write(file_obj)
+end = time.time()
+print("\t Started: {0} | Ended: {1}. ELAPSED: {2} ({3} per page)".format(start, end, end-start, (end-start)/page_count))
+print("\t Levenshtein Distance (~Accuracy): {0}".format(Levenshtein.ratio(extractPdfText(target_file), benchmark_text)*100))
+if remove_test_output:
+	os.remove(target_file)
+
+print("\n---MULTITHREAD: Testing string source, string target---")
+start = time.time()
+target_file=os.path.join(dirname, 'lilrawr.pdf')
+Multithreaded_OCRPDF(source=source_file, targetPath=target_file)
+end = time.time()
+print("\t Started: {0} | Ended: {1}. ELAPSED: {2} ({3} per page)".format(start, end, end-start, (end-start)/page_count))
+print("\t Levenshtein Distance (~Accuracy): {0}".format(Levenshtein.ratio(extractPdfText(target_file), benchmark_text)*100))
+if remove_test_output:
+	os.remove(target_file)
+
+print("\n---Testing byte obj source, byte obj return---")
+start = time.time()
+target_file=os.path.join(dirname, "tinyrawr.pdf")
+with open(source_file,'rb') as s:
+	file_obj = Multithreaded_OCRPDF(source=s.read())
+	with open(target_file, "wb+") as f:
+		f.write(file_obj)
+end = time.time()
+print("\t Started: {0} | Ended: {1}. ELAPSED: {2} ({3} per page)".format(start, end, end-start, (end-start)/page_count))
 print("\t Levenshtein Distance (~Accuracy): {0}".format(Levenshtein.ratio(extractPdfText(target_file), benchmark_text)*100))
 if remove_test_output:
 	os.remove(target_file)
@@ -56,28 +104,37 @@ if remove_test_output:
 ### Switch to test of OCRing of arbitrary page #. Load benchmark text for target page=2
 benchmark_text = open(os.path.join(dirname, "page2_text.txt")).read()
 
-print("---Testing byte obj source, byte obj return, and page=2---")
+print("\n---Testing byte obj source, byte obj return, and page=2---")
+start = time.time()
 target_file=os.path.join(dirname, "tinyrawr.pdf")
 with open(source_file,'rb') as s:
 	file_obj = OCRPDF(source=s.read(), page=2)
 	with open(target_file, "wb+") as f:
 		f.write(file_obj)
+end = time.time()
+print("\t Started: {0} | Ended: {1}. ELAPSED: {2} ({3} per page)".format(start, end, end-start, (end-start)/page_count))
 print("\t Levenshtein Distance (~Accuracy): {0}".format(Levenshtein.ratio(extractPdfText(target_file), benchmark_text)*100))
 if remove_test_output:
 	os.remove(target_file)
 
-print("---Testing string source, byte obj return, and page=2---")
+print("\n---Testing string source, byte obj return, and page=2---")
+start = time.time()
 file_obj = OCRPDF(source=source_file, page=2)
 target_file = os.path.join(dirname, "rawrjr.pdf")
 with open(target_file, "wb+") as f:
     f.write(file_obj)
-print("\tLevenshtein Distance (~Accuracy): {0}".format(Levenshtein.ratio(extractPdfText(target_file), benchmark_text)*100))
+end = time.time()
+print("\t Started: {0} | Ended: {1}. ELAPSED: {2} ({3} per page)".format(start, end, end-start, (end-start)/page_count))
+print("\t Levenshtein Distance (~Accuracy): {0}".format(Levenshtein.ratio(extractPdfText(target_file), benchmark_text)*100))
 if remove_test_output:
 	os.remove(target_file)
 
-print("---Testing string source, string target, and page=2---")
+print("\n---Testing string source, string target, and page=2---")
+start = time.time()
 target_file=os.path.join(dirname, 'lilrawr.pdf')
 OCRPDF(source=source_file, targetPath=target_file, page=2)
+end = time.time()
+print("\t Started: {0} | Ended: {1}. ELAPSED: {2} ({3} per page)".format(start, end, end-start, (end-start)/page_count))
 print("\t Levenshtein Distance (~Accuracy): {0}".format(Levenshtein.ratio(extractPdfText(target_file), benchmark_text)*100))
 if remove_test_output:
 	os.remove(target_file)
